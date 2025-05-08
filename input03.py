9378b50acd50878fff2c80db357373bbb960a5eb
Vinicius Sakaguti <yuitisakaguti@gmail.com>
2025-05-08 18:02:00 -0300

    Initial commit

---
+++ input03.py
@@ -0,0 +1,12 @@
+name = input("What is your name?: ")
+age =  int(input("How old are you?: "))
+
+print(F"Hello, {name}")
+print(f"You are {age} years old")
+
+width = float(input("Input the width of rectangle: "))
+length = float(input("Input the length of the rectangle: "))
+
+area = width * length
+
+print(f"The area is: {area}cm²") # alt 0178 to add cm²f
---
+++ typecasting02.py
@@ -0,0 +1,26 @@
+#Typecasting is the process of converting a variable from one data type to another
+
+name = "Vinícius"
+age = 20
+height = 1.70
+is_worker = True
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_worker))
+
+height = int(height)
+print(height)
+
+age = float(age)
+print(age)
+
+name = bool(name)
+print(name)
+print(type(name))
+
+is_worker = str(is_worker)
+print(is_worker)
+print(type(is_worker))
+ 
