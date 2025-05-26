4736a60007189ee46caad574e9fc63179bc14075
Vinicius Sakaguti <yuitisakaguti@gmail.com>
2025-05-26 17:59:17 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+primeiro_numero = float(input("Digite o primeiro número: "))
+segundo_numero = float(input("Digite o segundo número: "))
+operador = input("Escolha o operador desejado (+ - * /): ")
+
+if operador == "+":
+    resultado = primeiro_numero + segundo_numero
+    print(resultado)
+elif operador == "-":
+    resultado = primeiro_numero - segundo_numero
+    print(resultado)
+elif operador == "*":
+    resultado = primeiro_numero * segundo_numero
+    print(resultado)
+elif operador == "/":
+    resultado = primeiro_numero / segundo_numero
+    print(resultado)
+
+
+
+
+
